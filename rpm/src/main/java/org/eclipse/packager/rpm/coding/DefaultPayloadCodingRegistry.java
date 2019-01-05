7c903f89e9a4306525e333c2d5e97adc3cd55f54
Jens Reimann <jreimann@redhat.com>
2019-01-05 16:06:49 +0100

    Forgot to drop the registry, until we have something better

--- rpm/src/main/java/org/eclipse/packager/rpm/coding/DefaultPayloadCodingRegistry.java
+++ rpm/src/main/java/org/eclipse/packager/rpm/coding/DefaultPayloadCodingRegistry.java
@@ -1,16 +1,13 @@
-/**
- * Copyright (c) 2015, 2019 Contributors to the Eclipse Foundation
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- */
-
+ * Contributors:
+ *     Red Hat Inc - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.packager.rpm.coding;
 
 import java.io.IOException;
