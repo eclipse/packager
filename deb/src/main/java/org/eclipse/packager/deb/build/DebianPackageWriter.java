8a39cd240693626c312dd798a3154a123d05f236
Jens Reimann <jreimann@redhat.com>
2019-09-09 11:07:32 +0200

    Change license headers of the packager-deb to EPLv2

--- deb/src/main/java/org/eclipse/packager/deb/build/DebianPackageWriter.java
+++ deb/src/main/java/org/eclipse/packager/deb/build/DebianPackageWriter.java
@@ -1,15 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2014, 2019 IBH SYSTEMS GmbH and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * Copyright (c) 2014, 2019 Contributors to the Eclipse Foundation
  *
- * Contributors:
- *     IBH SYSTEMS GmbH - initial API and implementation
- *     M-Ezzat - code cleanup - squid:S2131
- *     Red Hat Inc
- *******************************************************************************/
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.packager.deb.build;
 
 import java.io.File;
