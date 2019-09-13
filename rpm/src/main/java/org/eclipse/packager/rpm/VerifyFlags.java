9c4243a70496504f473ffd6f69f33bd63f0cb7a5
Oliver Matz <oliver.matz@dataport.de>
2019-09-13 15:59:09 +0200

    constants for the VERIFY_* bits, comment
    Signed-off-by: Oliver Matz <oliver.matz@dataport.de>

--- rpm/src/main/java/org/eclipse/packager/rpm/VerifyFlags.java
+++ rpm/src/main/java/org/eclipse/packager/rpm/VerifyFlags.java
@@ -11,6 +11,8 @@ package org.eclipse.packager.rpm;
  */
 public enum VerifyFlags
 {
+   // The following constants control the verify flags.
+   // Each bit corresponds to an upper case character in the output of "rpm -V ..."
    MD5(   1 << 0), // '5'
    SIZE(  1 << 1), // 'S'
    LINKTO(1 << 2), // 'L'
@@ -19,11 +21,24 @@ public enum VerifyFlags
    MTIME( 1 << 5), // 'T'
    MODE(  1 << 6), // 'M'
    RDEV(  1 << 7), // 'D'
-   CAPS(  1 << 8); // 'P'
-   //  VERIFY_CONTEXTS = (1 << 15),  VERIFY_FILES = (1 << 16),  VERIFY_DEPS = (1 << 17),
-   //  VERIFY_SCRIPT = (1 << 18),  VERIFY_DIGEST = (1 << 19),
-   //  VERIFY_SIGNATURE = (1 << 20), VERIFY_PATCHES = (1 << 21), VERIFY_HDRCHK = (1 << 22), VERIFY_FOR_LIST = (1 << 23),
-   //  VERIFY_FOR_STATE = (1 << 24), VERIFY_FOR_DOCS = (1 << 25), VERIFY_FOR_CONFIG = (1 << 26), VERIFY_FOR_DUMPFILES = (1 << 27)
+   CAPS(  1 << 8), // 'P'
+
+   // The purpose of the following constants is not clear to me.
+   // Do they refer to the same bitmask? Oliver Matz
+   // see diskussion in https://github.com/eclipse/packager/pull/6
+   VERIFY_CONTEXTS      (1 << 15),
+   VERIFY_FILES         (1 << 16),
+   VERIFY_DEPS          (1 << 17),
+   VERIFY_SCRIPT        (1 << 18),
+   VERIFY_DIGEST        (1 << 19),
+   VERIFY_SIGNATURE     (1 << 20),
+   VERIFY_PATCHES       (1 << 21),
+   VERIFY_HDRCHK        (1 << 22),
+   VERIFY_FOR_LIST      (1 << 23),
+   VERIFY_FOR_STATE     (1 << 24),
+   VERIFY_FOR_DOCS      (1 << 25),
+   VERIFY_FOR_CONFIG    (1 << 26),
+   VERIFY_FOR_DUMPFILES (1 << 27);
 
    private final int value;
 
