788d757b5277a5122702795cee5d1edc97c5f2fe
Oliver Matz <oliver.matz@dataport.de>
2019-09-16 10:53:55 +0200

    add license header, remove javadoc author tag
    Signed-off-by: Oliver Matz <oliver.matz@dataport.de>

--- rpm/src/main/java/org/eclipse/packager/rpm/VerifyFlags.java
+++ rpm/src/main/java/org/eclipse/packager/rpm/VerifyFlags.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.packager.rpm;
 
 /**
@@ -7,7 +19,6 @@ package org.eclipse.packager.rpm;
  * It should rather be "VerifyFlag" (singular), but I leave it this way, cf. {@link FileFlags}.
  * The constants and their value are from http://ftp.rpm.org/api/4.14.0/group__rpmvf.html
  * @since 0.15.2
- * @author Oliver Matz
  */
 public enum VerifyFlags
 {
--- rpm/src/test/java/org/eclipse/packager/rpm/SetVerifyFlagsTest.java
+++ rpm/src/test/java/org/eclipse/packager/rpm/SetVerifyFlagsTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.packager.rpm;
 
 import org.eclipse.packager.rpm.app.Dumper;
@@ -24,7 +36,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * see https://github.com/ctron/rpm-builder/issues/41
- * @author Oliver Matz
  */
 class SetVerifyFlagsTest
 {
