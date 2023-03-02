f7a777bdf0ec91156d4ef03bb1414f51e1dd2506
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2023-03-02 11:35:13 +0100

    init tests

---
+++ rpm/src/test/java/org/eclipse/packager/rpm/signature/RawRpmFileSignatureProcessorTest.java
@@ -0,0 +1,12 @@
+package org.eclipse.packager.rpm.signature;
+
+import org.junit.jupiter.api.Test;
+
+public class RawRpmFileSignatureProcessorTest {
+    
+    @Test
+    public void test_signing_existing_rpm() {
+        
+    }
+
+}
