977a59ec196e3ebd27a2d738db0e1d6eddf63f60
Paul Schaub <vanitasvitae@fsfe.org>
2023-05-30 02:38:05 +0200

    Base RpmFileSignatureProcessor on new PgpSignatureProcessor

--- rpm/src/main/java/org/eclipse/packager/rpm/signature/RpmFileSignatureProcessor.java
+++ rpm/src/main/java/org/eclipse/packager/rpm/signature/RpmFileSignatureProcessor.java
@@ -32,7 +32,6 @@ import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.bouncycastle.openpgp.bc.BcPGPSecretKeyRing;
 import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyDecryptorBuilder;
 import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
-import org.eclipse.packager.rpm.HashAlgorithm;
 import org.eclipse.packager.rpm.RpmSignatureTag;
 import org.eclipse.packager.rpm.Rpms;
 import org.eclipse.packager.rpm.header.Header;
@@ -40,10 +39,13 @@ import org.eclipse.packager.rpm.header.Headers;
 import org.eclipse.packager.rpm.info.RpmInformation;
 import org.eclipse.packager.rpm.info.RpmInformations;
 import org.eclipse.packager.rpm.parse.RpmInputStream;
+import org.pgpainless.PGPainless;
+import org.pgpainless.key.protection.SecretKeyRingProtector;
+import org.pgpainless.util.Passphrase;
 
 /**
  * Sign existing RPM file by calling
- * {@link #perform(File, InputStream, String, OutputStream, HashAlgorithm)}
+ * {@link #perform(File, InputStream, String, OutputStream)}
  */
 public class RpmFileSignatureProcessor {
 
@@ -62,10 +64,9 @@ public class RpmFileSignatureProcessor {
      * @param passphrase : passphrase to decrypt the private key
      * @param out : {@link OutputStream} to write to
      * @throws IOException
-     * @throws PGPException
      */
-    public static void perform(File rpm, InputStream privateKeyIn, String passphrase, OutputStream out, HashAlgorithm hashAlgorithm)
-        throws IOException, PGPException {
+    public static void perform(File rpm, InputStream privateKeyIn, String passphrase, OutputStream out)
+        throws IOException {
 
         final long leadLength = 96;
         long signatureHeaderStart = 0L;
@@ -81,8 +82,9 @@ public class RpmFileSignatureProcessor {
             throw new IOException("The file " + rpm.getName() + " does not exist");
         }
 
-        // Extract private key
-        PGPPrivateKey privateKey = getPrivateKey(privateKeyIn, passphrase);
+        PGPSecretKeyRing secretKeys = PGPainless.readKeyRing().secretKeyRing(privateKeyIn);
+        SecretKeyRingProtector protector = passphrase == null ? SecretKeyRingProtector.unprotectedKeys() :
+            SecretKeyRingProtector.unlockAnyKeyWith(Passphrase.fromPassword(passphrase));
 
         // Get the information of the RPM
         try (RpmInputStream rpmIn = new RpmInputStream(new FileInputStream(rpm))) {
@@ -109,7 +111,7 @@ public class RpmFileSignatureProcessor {
             IOUtils.readFully(channelIn, payloadHeaderBuff);
             ByteBuffer payloadBuff = ByteBuffer.allocate((int) payloadSize);
             IOUtils.readFully(channelIn, payloadBuff);
-            signatureHeader = getSignature(privateKey, payloadHeaderBuff, payloadBuff, archiveSize, hashAlgorithm);
+            signatureHeader = getSignature(secretKeys, protector, payloadHeaderBuff, payloadBuff, archiveSize);
         }
 
         // Write to the OutputStream
@@ -127,18 +129,18 @@ public class RpmFileSignatureProcessor {
      * "https://rpm-software-management.github.io/rpm/manual/format.html">https://rpm-software-management.github.io/rpm/manual/format.html</a>
      * </p>
      *
-     * @param privateKey : private key already extracted
+     * @param secretKeys signing key
+     * @param protector signing key protector
      * @param payloadHeader : Payload's header as {@link ByteBuffer}
      * @param payload : Payload as {@link ByteBuffer}
      * @param archiveSize : archiveSize retrieved in {@link RpmInformation}
-     * @param hashAlgorithm
      * @return the signature header as a bytes array
      * @throws IOException
      */
-    private static byte[] getSignature(PGPPrivateKey privateKey, ByteBuffer payloadHeader, ByteBuffer payload,
-        long archiveSize, HashAlgorithm hashAlgorithm) throws IOException {
+    private static byte[] getSignature(PGPSecretKeyRing secretKeys, SecretKeyRingProtector protector, ByteBuffer payloadHeader, ByteBuffer payload,
+        long archiveSize) throws IOException {
         Header<RpmSignatureTag> signatureHeader = new Header<>();
-        List<SignatureProcessor> signatureProcessors = getSignatureProcessors(privateKey, hashAlgorithm);
+        List<SignatureProcessor> signatureProcessors = getSignatureProcessors(secretKeys, protector);
         payloadHeader.flip();
         payload.flip();
         for (SignatureProcessor processor : signatureProcessors) {
@@ -183,17 +185,18 @@ public class RpmFileSignatureProcessor {
      * {@link SignatureProcessors}
      * </p>
      *
-     * @param privateKey : the private key, already extracted
+     * @param secretKeys signing secret key
+     * @param protector protector to unlock the secret key
      * @return {@link List<SignatureProcessor>} of {@link SignatureProcessor}
      */
-    private static List<SignatureProcessor> getSignatureProcessors(PGPPrivateKey privateKey, HashAlgorithm hashAlgorithm) {
+    private static List<SignatureProcessor> getSignatureProcessors(PGPSecretKeyRing secretKeys, SecretKeyRingProtector protector) {
         List<SignatureProcessor> signatureProcessors = new ArrayList<>();
         signatureProcessors.add(SignatureProcessors.size());
         signatureProcessors.add(SignatureProcessors.sha256Header());
         signatureProcessors.add(SignatureProcessors.sha1Header());
         signatureProcessors.add(SignatureProcessors.md5());
         signatureProcessors.add(SignatureProcessors.payloadSize());
-        signatureProcessors.add(new RsaSignatureProcessor(privateKey, hashAlgorithm));
+        signatureProcessors.add(new PgpSignatureProcessor(secretKeys, protector));
         return signatureProcessors;
     }
 
